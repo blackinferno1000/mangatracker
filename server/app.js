8daa790c1957f64321d7a24d4836c5946abe9d9b
Sean Lightfoot <seanalexander360@gmail.com>
2020-09-15 19:19:47 -0500

    migrate to atlas

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const redis = require('redis');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/finalProject';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/mangatracker';
 
 const mongooseOptions = {
   useNewUrlParser: true,
