f4f2aa460d699b902dd0a62ebfd51e00995cfef4
Sean Lightfoot <seanalexander360@gmail.com>
2021-05-23 23:11:21 -0500

    redis updated

--- server/app.js
+++ server/app.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const express = require('express');
-const compression = require('compression');
-const favicon = require('serve-favicon');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const expressHandlebars = require('express-handlebars');
-const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
-const url = require('url');
-const csrf = require('csurf');
-const redis = require('redis');
+const path = require("path");
+const express = require("express");
+const compression = require("compression");
+const favicon = require("serve-favicon");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const expressHandlebars = require("express-handlebars");
+const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
+const url = require("url");
+const csrf = require("csurf");
+const redis = require("redis");
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URL || 'mongodb://localhost/mangatracker';
+const dbURL = process.env.MONGODB_URL || "mongodb://localhost/mangatracker";
 
 const mongooseOptions = {
   useNewUrlParser: true,
@@ -23,21 +23,21 @@ const mongooseOptions = {
 
 mongoose.connect(dbURL, mongooseOptions, (err) => {
   if (err) {
-    console.log('Could not connect to database');
+    console.log("Could not connect to database");
     throw err;
   }
 });
 
 let redisURL = {
-  hostname: 'redis-15618.c52.us-east-1-4.ec2.cloud.redislabs.com',
-  port: 15618,
+  hostname: "redis-15375.c10.us-east-1-4.ec2.cloud.redislabs.com",
+  port: 15375,
 };
 
-let redisPASS = 'nSzX8OfnfcYuNPrkXVyrRIJ3piqkuiHO';
+let redisPASS = "83l4CaGOpob9a7qEISyM0r2QBsVYG4rQ";
 if (process.env.REDISCLOUD_URL) {
   redisURL = url.parse(process.env.REDISCLOUD_URL);
   const passIndex = 1;
-  redisPASS = redisURL.auth.split(':')[passIndex];
+  redisPASS = redisURL.auth.split(":")[passIndex];
 }
 const redisClient = redis.createClient({
   host: redisURL.hostname,
@@ -45,38 +45,43 @@ const redisClient = redis.createClient({
   password: redisPASS,
 });
 
-const router = require('./router');
-
+const router = require("./router");
 
 const app = express();
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use("/assets", express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.ico`));
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 app.use(compression());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
-app.use(session({
-  key: 'sessionid',
-  store: new RedisStore({
-    client: redisClient,
-  }),
-  secret: 'so many secrets',
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-  },
-}));
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  session({
+    key: "sessionid",
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: "so many secrets",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+    },
+  })
+);
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/../views`);
 app.use(cookieParser());
 app.use(csrf());
 app.use((err, req, res, next) => {
-  if (err.code !== 'EBADCSRFTOKEN') { return next(err); }
+  if (err.code !== "EBADCSRFTOKEN") {
+    return next(err);
+  }
 
-  console.log('Missing CSRF token');
+  console.log("Missing CSRF token");
   return false;
 });
 
